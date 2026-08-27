edcb3d198d0bfab02d98a76839a0b9276638201b
Sven Haan <svenhaan@gmx.net>
2026-08-27 09:57:29 +0200

    Added Online Check Configuration

--- ScreentimeManagerCore/Configuration/OnlineCheckConfiguration.cs
+++ ScreentimeManagerCore/Configuration/OnlineCheckConfiguration.cs
@@ -1,11 +1,28 @@
-﻿using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using System.Net.NetworkInformation;
-using System.Threading;
+﻿using Microsoft.Extensions.Configuration;
 
 namespace ScreentimeManagerCore.Configuration
 {
+    /// <summary>
+    /// Represents the configuration for an Online-Check Service.
+    /// </summary>
     public class OnlineCheckConfiguration
     {
+        /// <summary>
+        /// Hostname of the host to ping
+        /// </summary>
+        [ConfigurationKeyName("PING_HOST")]
+        public virtual string Host { get; set; } = "";
+
+        /// <summary>
+        /// Timeout for Online-Check in Milliseconds.
+        /// </summary>
+        [ConfigurationKeyName("PING_TIMEOUT_MS")]
+        public virtual int Timeout { get; set; } = 200;
+
+        /// <summary>
+        /// Interval to repeat Online-Check in Milliseconds
+        /// </summary>
+        [ConfigurationKeyName("PING_INTERVAL_MS")]
+        public virtual int CheckInterval { get; set; } = 30000;
     }
 }
